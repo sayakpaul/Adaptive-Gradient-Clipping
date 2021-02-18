5ab6d65f2e331e20b3396b0f3995e81203e49630
Sayak Paul <spsayakpaul@gmail.com>
2021-02-18 12:10:19 +0530

    agc modifications

--- agc.py
+++ agc.py
@@ -37,7 +37,6 @@ def adaptive_clip_grad(parameters, gradients, clip_factor=0.01,
         p_norm = unitwise_norm(params)
         max_norm = tf.math.maximum(p_norm, eps) * clip_factor
         grad_norm = unitwise_norm(grads)
-
         clipped_grad = grads * (
                     max_norm / tf.math.maximum(grad_norm, 1e-6))
         new_grad = tf.where(grad_norm < max_norm, grads,
